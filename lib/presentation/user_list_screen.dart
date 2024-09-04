75190a589a1d691abc3bf2b2ed533708ff1d59af
jaekyungyou <youjk96@gmail.com>
2024-09-04 15:05:35 +0900

    [셋팅] dio_api_service 생성, cachedImageNetwork 라이브러리 추가

--- lib/presentation/user_list_screen.dart
+++ lib/presentation/user_list_screen.dart
@@ -5,6 +5,12 @@ class UserListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [Text('user list screen')],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/service/dio_api_service.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+
+class DioApiBase {
+  DioApiBase._privateConstructor();
+
+  static final _instance = DioApiBase._privateConstructor();
+  final Dio dio = Dio();
+
+  factory DioApiBase() {
+    return _instance;
+  }
+
+  Future<void> init() async {
+    dio.options.baseUrl = 'http://3.35.9.192:8282/api/viva';
+    dio.options.connectTimeout = const Duration(seconds: 5);
+    dio.options.receiveTimeout = const Duration(seconds: 3);
+  }
+
+  Future<Response> get(String? path, Map<String, dynamic>? queryParam) async {
+    Response? res;
+    try {
+      res = await dio.get(path ?? '', queryParameters: queryParam);
+      return res;
+    } catch (e) {
+      print("DIO GET ERROR : $e");
+      rethrow;
+    }
+  }
+}
